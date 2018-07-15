a9307aae41c371fca0f280d538c24d1adfcd3815
James Hobson <james.hobson@oriel.ox.ac.uk>
2018-07-15 19:29:52 +0100

    Started to build searchable datatype

--- search.cpp
+++ search.cpp
@@ -2,34 +2,60 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct tre {
-    void *data;
-    struct tre *next[26];
-};
-
-typedef struct tre Tre;
-
-void _addSearchableData(Tre *root, char *name, void *data) {
-    if (*name = '\0') {
+Tre* _addSearchableData(Tre *root, char *name, Data *data) {
+    if (*name == '\0') {
         root->data = data;
-        return;
+        return root;
     }
+    while (*name - (unsigned char)'a' < 0 || *name - (unsigned char)'a' >= 26) name++; // Ignore non-ascii
     unsigned char index = *name - (unsigned char)'a';
-    if (root->next[index] == NULL)
+    if (root->next[index] == NULL) {
         root->next[index] = (Tre*)malloc(sizeof(Tre));
-    _addSearchableData(root->next[index], name + 1, data);
+    }
+    return _addSearchableData(root->next[index], name + 1, data);
 }
 
-void addSearchableData(Tre *root, char *name, void *data) {
+Tre* addSearchableData(Tre *root, char *name, Data *data) {
     int length = strlen(name);
     int i = 0; while (name[i] != ' ' && i < length) i++;
-    if (name[i] == ' ') addSearchableData(root, name + i + 1, data);
-    _addSearchableData(root, name, data);
+    if (name[i] == ' ') addSearchableData(root, name + i + 1, data); //seporates by space (kinda).
+    return _addSearchableData(root, name, data);
 }
 
-Tre *searchableIndex = NULL;
+Tre* _searchFor(Tre *root, char *name) {
+    if (*name == '\0') return root;
+    while (*name - (unsigned char)'a' < 0 || *name - (unsigned char)'a' >= 26) name++; // Ignore non-ascii
+    unsigned char index = *name - (unsigned char)'a';
+    if (root->next[index] == NULL) return NULL;
+    return _searchFor(root->next[index], name + 1);
+}
+
+QList<QString> flattern(Tre *root) {
+    QList<QString> data;
+    data << *(QString*)root;
+    for(int i = 0; i < 26; i++)
+        if (root->next[i] != NULL)
+            data += flattern(root->next[i]);
+    return data;
+}
 
 Search::Search()
 {
-    searchableIndex = (Tre*)malloc(sizeof(Tre));
+    searchableData = (Tre*)malloc(sizeof(Tre));
+}
+
+void Search::addData(QString name, Data *data)
+{
+    Tre *inserted = addSearchableData(searchableData, name.toLower().toLatin1().data(), data);
+    inserted->data->name = &name;
+}
+
+Tre* Search::searchForTre(QString name) {
+    return _searchFor(searchableData, name.toLower().toLatin1().data());
+}
+
+QList<QString> Search::searchForList(QString name) {
+    Tre *results = searchForTre(name);
+    if (results == NULL) QList<QString>();
+    return flattern(results);
 }
--- search.h
+++ search.h
@@ -1,11 +1,29 @@
 #ifndef SEARCH_H
 #define SEARCH_H
+#include <QString>
+#include <QList>
 
+struct Data {
+    QString *name;
+    void *data;
+};
+
+struct Tre {
+    Data *data;
+    struct Tre *next[26];
+};
 
 class Search
 {
 public:
     Search();
+    void addData(QString name, Data *data);
+    Tre* searchForTre(QString);
+    QList<QString> searchForList(QString);
+
+private:
+
+    struct Tre *searchableData;
 };
 
 #endif // SEARCH_H
