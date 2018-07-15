c6fab09b8c925b46de400f5d2a7e263b7e0ec48b
James Hobson <james.hobson@oriel.ox.ac.uk>
2018-07-15 14:18:00 +0100

    Added ability to find hospital and school files from UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QStandardPaths>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +14,16 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionImport_Schools_triggered() //Gets full path to file
+{
+    QString documentsLocation = QStandardPaths::locate(QStandardPaths::DocumentsLocation, QString(), QStandardPaths::LocateDirectory);
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Schools File"), documentsLocation, tr("JSON Files (*.json)"));
+}
+
+void MainWindow::on_actionImport_Hospitals_triggered()
+{
+    QString documentsLocation = QStandardPaths::locate(QStandardPaths::DocumentsLocation, QString(), QStandardPaths::LocateDirectory);
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Schools File"), documentsLocation, tr("CSV Files (*.csv)"));
 }
