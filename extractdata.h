95e240ed89415f37463aeeca309a732486ab90cc
James Hobson <james.hobson@arm.com>
2018-07-20 16:23:27 +0100

    Merge cleanup

--- extractdata.h
+++ extractdata.h
@@ -4,32 +4,8 @@
 #endif // EXTARCTDATA_H
 
 #include <QList>
-
-struct School
-{
-    QString id;
-    int colour[3];
-    QString name;
-    double xCoordinate;
-    double yCoordinate;
-
-};
-
-struct HealthFacility
-{
-  QString name;
-  QString type;
-  QString source;
-  QString sourceURL;
-  QString dateModified;
-  QString id;
-  QString completeness;
-  QString version;
-  QString upstream;
-  QString what3words;
-  double xCoordinate;
-  double yCoordinate;
-};
+#include <school.h>
+#include <healthfacility.h>
 
 QList<School> getSchools(QString filepath);
 QList<HealthFacility> getHealthFacilities(QString filepath);
