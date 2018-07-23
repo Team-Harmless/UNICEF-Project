0e6c5ebfb5b57eb02cdb0258794b38f48b23d3ca
Milen Orfeev <milen.orfeev@abv.bg>
2018-07-23 16:07:22 +0100

    Now testing data structure

--- main.cpp
+++ main.cpp
@@ -10,19 +10,36 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    /*
-    qDebug() << QDir::current();
-
-    Quad placesQuad = Quad::createUsingQlist();
 
 
     QList<Place *> schoolsList = Extractor::getSchools("schools.json");
 
-    Node b(schoolsList[0]);
+    Place * firstSchool  = schoolsList.takeAt(0);
+    double minLongtitude = firstSchool->coord.longitude();
+    double maxLongtitude = firstSchool->coord.longitude();
+    double minLatitude = firstSchool->coord.latitude();
+    double maxLatitude = firstSchool->coord.latitude();
 
-    Quad oneLocationQuad(Point(-100,-100), Point(100,100));
+    qDebug() << "len is " << schoolsList.size();
+    qDebug() << "first x is" << firstSchool->coord.latitude() <<endl;
+    foreach (Place* schoolPointer, schoolsList)
+    {
+        qDebug() << "x is" << schoolPointer->coord.latitude()<< endl;
+        if (minLongtitude < schoolPointer->coord.longitude())
+            minLongtitude = schoolPointer->coord.longitude();
+        if (maxLongtitude > schoolPointer->coord.longitude())
+            maxLongtitude = schoolPointer->coord.longitude();
+        if (minLatitude < schoolPointer->coord.latitude())
+            minLatitude = schoolPointer->coord.latitude();
+        if (maxLatitude > schoolPointer->coord.latitude())
+            maxLatitude = schoolPointer->coord.latitude();
+    } // foreach
 
-    oneLocationQuad.insert(&b);
+    qDebug() << "Min and max x are " << minLongtitude << " " <<maxLongtitude<<endl;
+    qDebug() << "Min and max y are " << minLatitude << " " <<maxLatitude<<endl;
+
+    /*
+    Quad placesQuad = Quad::();
 
     cout << "Original coordinate is" <<
             schoolsList[0]->xCoordinate<< "\n";
@@ -36,16 +53,7 @@ int main(int argc, char *argv[])
 
     qDebug() << "Searhing in small quad returns coordinate as " <<
         oneLocationQuad.search(b.pos)->pos.x << "\n";
-
-    Place copy = *schoolsList[0];
-    copy.xCoordinate = -75.3456;
-    copy.yCoordinate = 13.436;
-    Node anotherb(&copy);
-
-    oneLocationQuad.insert(&anotherb);
-
-    qDebug() <<oneLocationQuad.search(anotherb.pos)->pos.x<<endl;
-    */
+        */
 
     return a.exec();
 }
